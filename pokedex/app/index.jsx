91f645383873db8a1cffdd8207285ba0c6e07cd0
LucasSpricigo <lucasspricigo21@gmail.com>
2026-06-10 14:20:35 -0300

    parei na pagina 40

---
+++ pokedex/app/index.jsx
@@ -0,0 +1,22 @@
+import { FlatList, StyleSheet, Text, View } from "react-native";
+import Header from '@/components/header';
+import { pokemons } from "@/data/pokemons"
+
+export default function Index () {
+    return (
+        <View style={styles.container}>
+            <Header title="Pokédex"/>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create ({
+    container: {
+        flex: 1,
+        backgroundColor: "#fff",
+        flexDirection: "column",
+    },
+    cards: {
+        padding: 15,
+    },
+});
