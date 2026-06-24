524a02c7b37b92338b72d164002dead5cff3437b
LucasSpricigo <lucasspricigo21@gmail.com>
2026-06-24 14:56:18 -0300

    projeto "finalizado", apresenta erros não identificados

--- pokedex/app/index.jsx
+++ pokedex/app/index.jsx
@@ -1,6 +1,6 @@
 import { FlatList, StyleSheet, Text, View } from "react-native";
-import { pokemons } from "@/data/pokemons"
 import Header from '@/components/Header';
+import { pokemons } from "@/data/pokemons"
 import Card from "../components/Card"
 
 export default function Index () {
